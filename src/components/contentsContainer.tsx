2a981e3614b14b1b5797bf95f1ea361a7d97e054
gotaek <64013443+gotaek@users.noreply.github.com>
2023-01-17 09:32:02 +0900

    add an animation and edit location of font

--- src/components/contentsContainer.tsx
+++ src/components/contentsContainer.tsx
@@ -3,12 +3,12 @@ import React from 'react';
 export default function ContentsContainer() {
   return (
     <div className="grid">
-      <span className="one">sirs.</span>
+      <div className="one">sirs.</div>
       <span className="two">어떤 사람인가</span>
       <span className="three">어떤 사람이 될 것인가</span>
-      <span className="four">irs.</span>
+      <div className="four">irs.</div>
       <span className="five">어떤 것을 했는가</span>
-      <span className="six">ir.</span>
+      <div className="six">ir.</div>
     </div>
   );
 }
