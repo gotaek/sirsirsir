964c759b0c6d342456775973c654ad220ee6da3b
gotaek <64013443+gotaek@users.noreply.github.com>
2023-01-26 13:57:28 +0900

    feat: create a new function opening and closing modal

--- src/components/contentsContainer.tsx
+++ src/components/contentsContainer.tsx
@@ -1,14 +1,57 @@
-import React from 'react';
+import React, { useRef, useState, useEffect, RefObject } from 'react';
+import ModalContainer from './modalContainer';
 
 export default function ContentsContainer() {
+  // const [divPosition, setDivPosition] = useState<{ x: number; y: number }>({ x: 0, y: 0 });
+  const [showModal, setShowModal] = useState<boolean>(false);
+  const buttonRef = useRef<HTMLButtonElement>(null);
+  const modalRef: RefObject<HTMLDivElement> = useRef(null);
+
+  const openModal = () => {
+    if (buttonRef.current) {
+      const rect = buttonRef.current.getBoundingClientRect();
+      console.log(`Button's position: Left: ${rect.left}, Top: ${rect.top}`);
+      // setModalPosition({ x: rect.left, y: rect.top });
+    }
+    setShowModal(!showModal);
+  };
+
+  const handleClickOutSide = (e: MouseEvent) => {
+    console.log(!modalRef.current || !modalRef.current.contains(e.target as Node));
+    if (showModal && (!modalRef.current || !modalRef.current.contains(e.target as Node))) {
+      setShowModal(false);
+    }
+  };
+
+  useEffect(() => {
+    if (showModal) {
+      document.addEventListener('mousedown', handleClickOutSide);
+      return () => {
+        document.removeEventListener('mousedown', handleClickOutSide);
+      };
+    }
+    return undefined;
+  }, [showModal]);
+
   return (
     <div className="grid">
-      <div className="one">sirs.</div>
+      <button className="one" ref={buttonRef} type="button" onClick={openModal}>
+        sirs.
+      </button>
+      {showModal && (
+        <div ref={modalRef}>
+          <ModalContainer />
+        </div>
+      )}
       <span className="two">어떤 사람인가</span>
       <span className="three">어떤 것을 했는가</span>
-      <div className="four">irs.</div>
+      <button className="four" type="button">
+        irs.
+      </button>
       <span className="five">어떤 사람이 될 것인가</span>
-      <div className="six">ir.</div>
+      <button className="six" type="button">
+        ir.
+      </button>
     </div>
   );
 }
---
+++ src/components/modalContainer.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function ModalContainer() {
+  return <div className="modalContainer"> this is modal </div>;
+}
